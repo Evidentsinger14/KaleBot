7d9ace11630d0359815613bf4892345ddb16bca1
Evidentsinger14 <Cdelancey186423@gmail.com>
2022-03-26 23:13:42 -0400

    lets see what i can break

--- commands/steal.js
+++ commands/steal.js
@@ -2,14 +2,19 @@ const { Client, Message, MessageEmbed, Util } = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
+    const isPermitted = message.member.roles.cache.has(process.env.BOT_STAFF)
+    
+    if(!isPermitted){
+        message.channel.send("You are not permitted to use that");
+        return;
+    }
+
     const embed = new MessageEmbed()
     .setColor("DARK_RED")
     .setDescription(":x: Incorrect format\n Correct format: " + process.env.PREFIX + "steal <Emoji>")
 
     if (!args.length){
-    return message.channel.send(
-        { embeds: [embed] }
-        );
+    return message.channel.send({ embeds: [embed] });
     }
 
     for (const raw_emoji of args){
@@ -22,10 +27,12 @@ module.exports.run = async (client, message, args) =>{
                 message.guild.emojis.create(url, parsed_emoji.name).then((emoji) => {
                     message.channel.send(`Added ${emoji.url}`);
                 });
-                
+
             } catch (error) {
+
                 message.channel.send("Could not add emoji");
                 return;
+
             }
         }
     }
--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-const discord = require("discord.js");
+const Discord = require("discord.js");
 require("dotenv").config();
+const fs = require("fs");
 
-const client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user?.username}`);
-})
+module.exports.client = client; 
 
-client.on("messageCreate", async message => {
-    const prefix = process.env.PREFIX;
-    if(message.author.bot || !message.content.startsWith(prefix)) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const cmd = args.shift().toLowerCase();
+client.events = new Discord.Collection();
 
-    // Command Handler
+// Event Handler
+fs.readdirSync("./events/").forEach(file => {
+    var jsFiles = fs.readdirSync("./events/").filter(f => f.split(".").pop() === "js");
+    if ( jsFiles.length <= 0 ) return console.log("[EVENT HANDLER] - Can't find any events.");
+    let check = false;
+    jsFiles.forEach(event => {
+        const eventGet = require(`./events/${event}`);
+        try{
+            client.events.set(eventGet.name, eventGet);
+        } catch (error) {
+            return console.error(error);
+        }
+    })
+});
 
-    try {
-        delete require.cache[require.resolve(`../commands/${cmd}.js`)];
 
-        const command_file = require(`../commands/${cmd}.js`);
-
-        command_file.run(client, message, args);
-    } catch (error){
-        
-    }
-})
 
 
 client.login(process.env.TOKEN);
+
+
+// client.on("messageCreate", async message => {
+//     const prefix = process.env.PREFIX;
+//     if(message.author.bot || !message.content.startsWith(prefix)) return;
+//     const args = message.content.slice(prefix.length).split(/ +/);
+//     const cmd = args.shift().toLowerCase();
+
+//     // Command Handler
+
+//     try {
+//         delete require.cache[require.resolve(`../commands/${cmd}.js`)];
+
+//         const command_file = require(`../commands/${cmd}.js`);
+
+//         command_file.run(client, message, args);
+//     } catch (error){
+        
+//     }
+// })
