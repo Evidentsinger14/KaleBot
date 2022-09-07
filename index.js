947a45773af90fb424245963a1981988d4ccb4bc
Evidentsinger14 <46200123+Evidentsinger14@users.noreply.github.com>
2022-09-07 12:50:30 -0400

    i might need this, but until then i guess i'll remove it

--- index.js
+++ index.js
@@ -28,23 +28,3 @@ fs.readdirSync("./events/").forEach(file => {
 
 client.login(process.env.TOKEN);
 console.log("Bot Ready...");
-
-
-// client.on("messageCreate", async message => {
-//     const prefix = process.env.PREFIX;
-//     if(message.author.bot || !message.content.startsWith(prefix)) return;
-//     const args = message.content.slice(prefix.length).split(/ +/);
-//     const cmd = args.shift().toLowerCase();
-
-//     // Command Handler
-
-//     try {
-//         delete require.cache[require.resolve(`../commands/${cmd}.js`)];
-
-//         const command_file = require(`../commands/${cmd}.js`);
-
-//         command_file.run(client, message, args);
-//     } catch (error){
-        
-//     }
-// })
