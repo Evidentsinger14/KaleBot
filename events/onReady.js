6cccc4ae453945149d57e99a8a54fe7ef9f8562f
Evidentsinger14 <Cdelancey186423@gmail.com>
2023-01-29 20:34:15 -0500

    add HTTPS call for Uptime Bot

--- events/onReady.js
+++ events/onReady.js
@@ -1,5 +1,13 @@
 const { client } = require("../index.js");
+const https = require('https');
+const domain = process.env.PUSH_URL
+
 
 client.on("ready", async () => {
+    setInterval(function() {
+        https.get(`${domain}`, () =>{
+            // Don't really need to do anything, just want to make sure that this will actually keep an uptime.
+        });
+    }, 4 * 10000);
     console.log("Bot Ready...");
 })
