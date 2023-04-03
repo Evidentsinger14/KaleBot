5e317af01e8a02ef48ee6cddbd56783bb2f3d719
Evidentsinger14 <46200123+Evidentsinger14@users.noreply.github.com>
2023-04-02 22:50:29 -0400

    Update onReady.js

--- events/onReady.js
+++ events/onReady.js
@@ -4,10 +4,19 @@ const domain = process.env.PUSH_URL
 
 
 client.on("ready", async () => {
-    setInterval(function() {
-        https.get(`${domain}`, () =>{
-            // Don't really need to do anything, just want to make sure that this will actually keep an uptime.
-        });
-    }, 4 * 10000);
+    
+ let check = true;
+        setInterval(function() {
+            if(check){
+                try{
+                    https.get(`${domain}`, () =>{
+                        // Don't really need to do anything, just want to make sure that this will actually keep an uptime.
+                    });
+                } catch (error){
+                    check = false;
+                }
+            }
+        }, 4 * 10000);
+    
     console.log("Bot Ready...");
 })
